39748816708a179f22c7a826c719ffdd92a46a8b
yakshitatomar16-cell <yakshitatomar16-cell@users.noreply.github.com>
2026-07-08 13:27:25 +0530

    Removed previous week tasks from week7 project

--- week4-weather-app/js/weatherService.js
+++
@@ -1,7 +0,0 @@
-function getWeather(city) {
-  return weatherData[city] || null;
-}
-
-function celsiusToFahrenheit(temp) {
-  return Math.round((temp * 9) / 5 + 32);
-}
--- week6-spring-blog-api/src/main/java/com/blogapi/config/SwaggerConfig.java
+++
@@ -1,5 +0,0 @@
-package main.java.com.blogapi.config;
-
-public class SwaggerConfig {
-    
-}
