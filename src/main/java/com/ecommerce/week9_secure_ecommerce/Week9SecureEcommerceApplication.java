712abca6875c1d56bc6fad62bb97afa0e9ed18da
yakshitatomar16-cell <yakshitatomar16-cell@users.noreply.github.com>
2026-07-09 11:51:57 +0530

    Week 9 Secure Ecommerce Project

---
+++ src/main/java/com/ecommerce/week9_secure_ecommerce/Week9SecureEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.week9_secure_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week9SecureEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week9SecureEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/week9_secure_ecommerce/Week9SecureEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.week9_secure_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week9SecureEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
